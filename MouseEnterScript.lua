4c2233245e02d1b2c5710fb52f94cfe0c48d39f9
CpxxQ <113293521+CpxxQ@users.noreply.github.com>
2022-09-11 15:11:17 +0200

    Update MouseEnterScript.lua

--- MouseEnterScript.lua
+++ MouseEnterScript.lua
@@ -1,9 +1,9 @@
 local button = script.parent
 
 button.MouseEnter:Connect(function()
-	button.BackgroundColor3 = Color3.fromRGB(163, 163, 163	)
+--	script here
 end)
 
 button.MouseLeave:Connect(function()
-	button.BackgroundColor3 = Color3.fromRGB(255,255,255)
+--	script here
 end)
