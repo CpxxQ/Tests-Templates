807344134640f463504a4285d8c54f5d9b0bd187
CpxxQ <113293521+CpxxQ@users.noreply.github.com>
2022-09-11 15:45:30 +0200

    Create SpecificPlayerAdded.lua

---
+++ SpecificPlayerAdded.lua
@@ -0,0 +1,12 @@
+game.Players.PlayerAdded:Connect(function(plr)
+	local UserID = {ID, ID, ID}
+	if table.find(UserID, plr.UserId) then
+		--[[
+		Script Here
+		--]]
+	else
+		--[[
+		Script Here
+		--]]
+	end
+end)
