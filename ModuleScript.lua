2301c4d3a22610459507a25057867a341c4454c6
CpxxQ <113293521+CpxxQ@users.noreply.github.com>
2022-09-12 21:56:50 +0200

    Create ModuleScript.lua

---
+++ ModuleScript.lua
@@ -0,0 +1,15 @@
+-- Module Script
+
+local module = {}
+
+function module.x()
+    -- Code Here
+end
+
+return module
+
+-- Script
+
+local Module = require(script.Parent.ModuleScript) -- Get service of ModuleScript
+
+Module.x()
